6eaa2bca80b77f54575a46d93338110efa6cf84d
Abdelrahman El-Adawy <adawy@oroboro.studio>
2020-05-21 23:58:06 +0200

    add basic server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const basicCallback = () => console.log(`server has started on port ${PORT}`);
+
+server.listen(PORT, basicCallback);
