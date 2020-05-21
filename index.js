ae212ab6d45405d170ed5e0f8b26e72d4bf7f3e6
Abdelrahman El-Adawy <adawy@oroboro.studio>
2020-05-22 00:10:52 +0200

    add basic router

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+
+app.use(router);
 
 const basicCallback = () => console.log(`server has started on port ${PORT}`);
 
