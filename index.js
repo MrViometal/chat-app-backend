6c773cc9f38f9bfccfd4458a0c15ef8b4bf2be81
Abdelrahman El-Adawy <adawy@oroboro.studio>
2020-05-22 07:18:40 +0200

    adding admin messages on new user connection, and user's functions

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
+//Import statements with require because FML
 const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
 
+//functions
+const { addUser, removeUser, getUser, getUserInRoom } = require('./users');
+
+//Port on localhost
 const PORT = process.env.PORT || 5000;
 
+//from router.js
 const router = require('./router');
 
+//start server and io sockets
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+//* watching socket on connection
 io.on('connection', (socket) => {
-  console.log('we have a new connection');
-
+  //
+  // When a socket has Join
   socket.on('join', ({ name, room }, callback) => {
-    console.log({ name, room });
+    //
+    // destructuring error and user from add user function
+    const { error, user } = addUser({
+      id: socket.id,
+      name,
+      room,
+    });
+
+    if (error) return callback(error);
+
+    //to let the user know they joined a room
+    //admin generated messages = 'message'
+    socket.emit('message', {
+      user: 'admin',
+      text: `${user.name} welcome to the room ${user.room}`,
+    });
+
+    //to broadcast to everyone else that a new user has joined
+    socket.broadcast.to(user.room).emit('message', {
+      user: 'admin',
+      text: `${user.name} has joined`,
+    });
+
+    //if no error, user joins room
+    socket.join(user.room);
+
+    callback();
   });
 
+  //* watching socket on disconnection
   socket.on('disconnect', () => {
     console.log('user had left!!!');
   });
